a182191d2c620e2d0606fffbf7c6ec6228bf34d5
Mikolaj Habryn <dichro@rcpt.to>
2013-02-08 00:32:37 -0500

    Switch packages for app publishing.

---
+++ src/to/rcpt/lamplighter/SingleLightActivity.java
@@ -0,0 +1,12 @@
+package to.rcpt.lamplighter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SingleLightActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_single_light);
+	}
+}
