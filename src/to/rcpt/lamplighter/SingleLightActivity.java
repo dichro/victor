e1b57c36cb5d8f4c7bd691d595fdb45ad9c861bb
Mikolaj Habryn <dichro@rcpt.to>
2013-02-09 18:26:44 -0500

    inherit from SingleLightActivity instead

--- src/to/rcpt/lamplighter/SingleLightActivity.java
+++ src/to/rcpt/lamplighter/SingleLightActivity.java
@@ -4,9 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class SingleLightActivity extends Activity {
+	private final int layout;
+
+	public SingleLightActivity(int layout) {
+		this.layout = layout;
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_single_light);
+		setContentView(layout);
 	}
 }
---
+++ src/to/rcpt/lamplighter/miki/LivingRoomActivity.java
@@ -0,0 +1,10 @@
+package to.rcpt.lamplighter.miki;
+
+import to.rcpt.lamplighter.R;
+import to.rcpt.lamplighter.SingleLightActivity;
+
+public class LivingRoomActivity extends SingleLightActivity {
+	public LivingRoomActivity() {
+		super(R.layout.activity_single_light);
+	}
+}
